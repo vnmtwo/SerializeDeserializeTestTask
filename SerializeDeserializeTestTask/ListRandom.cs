11cb2a1d5cd0f2f64715a45ea9d664518010d43a
vnm <alexey.beljakov@gmail.com>
2022-01-28 15:02:42 +0300

    second version

--- SerializeDeserializeTestTask/ListRandom.cs
+++ SerializeDeserializeTestTask/ListRandom.cs
@@ -10,6 +10,11 @@ namespace SerializeDeserializeTestTask
         public ListNode Next;
         public ListNode Random; // произвольный элемент внутри списка
         public string Data;
+
+        public ListNode(string value)
+        {
+            Data = value;
+        }
     }
 
     //struct: 
@@ -19,7 +24,7 @@ namespace SerializeDeserializeTestTask
     //datalength: int
     //data:string?
 
-    internal struct TempNode
+    internal class TempNode
     {
         public ListNode Node;
         public int Next, Previous, Random;
@@ -27,11 +32,132 @@ namespace SerializeDeserializeTestTask
 
     public class ListRandom
     {
-        public ListNode Head;
-        public ListNode Tail;
-        public int Count; // change minStreamLength if you will change type, because i cant use marshalling
-
         private const int minStreamLength = (sizeof(int) * 4) * 2 + sizeof(int); //struct*2 + Count;
+
+        private List<ListNode> Store;
+
+        private ListNode _head;
+        public ListNode Head
+        {
+            get
+            {
+                return _head;
+            }
+            set
+            {
+                if (Store.Contains(value.Next))
+                {
+                    int i = Store.IndexOf(value.Next);
+                    if (Store[i].Previous == value)
+                    {
+                        int indx = Store.IndexOf(_head);
+                        Store[indx] = value;
+                        _head = Store[indx];
+                        return;
+                    }
+                }
+                throw new Exception("Incorrect head");
+            }
+        }
+
+        private ListNode _tail;
+        public ListNode Tail
+        {
+            get
+            {
+                return _tail;
+            }
+            set
+            {
+                if (Store.Contains(value.Previous))
+                {
+                    int i = Store.IndexOf(value.Previous);
+                    if (Store[i].Next == value)
+                    {
+                        int indx = Store.IndexOf(_tail);
+                        Store[indx] = value;
+                        _head = Store[indx];
+                        return;
+                    }
+                }
+                throw new Exception("Incorrect tail");
+
+            }
+        }
+
+        public int Count // change minStreamLength if you will change type, because i cant use marshalling
+        {
+            get
+            {
+                return Store.Count;
+            }
+            private set
+            {
+
+            }
+        }
+
+        private Random rnd;
+
+        public ListRandom()
+        {
+            Init();
+            var l = new List<string>();
+        }
+
+        public void Init()
+        {
+            Store = new List<ListNode>();
+            rnd = new Random();
+
+            Store.Add(new ListNode(null));
+            Store.Add(new ListNode(null));
+
+            _head = Store[0];
+            _tail = Store[1];
+
+            _head.Next = _tail;
+            _head.Random = rnd.Next(2) == 0 ? null : _tail;
+            _head.Previous = null;
+
+            _tail.Next = null;
+            _tail.Random = rnd.Next(2) == 0 ? null : _head;
+            _tail.Previous = _head;
+        }
+
+        public void InsertElement(int index, string value)
+        {
+            if (index <= 0 || index >= Store.Count)
+                throw new ArgumentOutOfRangeException();
+            ListNode node = new ListNode(value);
+
+            ListNode currentNode = Head;
+            ListNode nextNode = null;
+            int c = 0;
+            do
+            {
+                nextNode = currentNode.Next;
+                if (index == c + 1)
+                {
+                    currentNode.Next = node;
+                    node.Previous = currentNode;
+                }
+                if (index == c)
+                {
+                    currentNode.Previous = node;
+                    node.Next = currentNode;
+                    break;
+                }
+                c++;
+                currentNode = nextNode;
+            } while (nextNode != null);
+
+            int i = rnd.Next(Count - 1) - 1;
+            if (i < 0) node.Random = null;
+            else node.Random = Store[i];
+
+            Store.Add(node);
+        }
 
         public void Serialize(Stream s)
         {
@@ -40,31 +166,21 @@ namespace SerializeDeserializeTestTask
             s.Write(BitConverter.GetBytes(Count));
             if (Count > 0)
             {
-                var indexes = new Dictionary<ListNode, int>();
-                int c = 0;
-                ListNode currentNode = Head;
-                do
-                {
-                    indexes.Add(currentNode, c++);
-                    currentNode = currentNode.Next;
-                } while (currentNode != null);
-
-                currentNode = Head;
-                do
+                foreach (var cn in Store)
                 {
                     int prev, next, random, datalength;
 
-                    if (currentNode.Previous == null) prev = -1;
-                    else prev = indexes[currentNode.Previous];
+                    if (cn.Previous == null) prev = -1;
+                    else prev = Store.IndexOf(cn.Previous);
 
-                    if (currentNode.Next == null) next = -1;
-                    else next = indexes[currentNode.Next];
+                    if (cn.Next == null) next = -1;
+                    else next = Store.IndexOf(cn.Next);
 
-                    if (currentNode.Random == null) random = -1;
-                    else random = indexes[currentNode.Random];
+                    if (cn.Random == null) random = -1;
+                    else random = Store.IndexOf(cn.Random);
 
-                    if (currentNode.Data == null) datalength = -1;
-                    else datalength = currentNode.Data.Length;
+                    if (cn.Data == null) datalength = -1;
+                    else datalength = cn.Data.Length;
 
                     s.Write(BitConverter.GetBytes(prev));
                     s.Write(BitConverter.GetBytes(next));
@@ -73,19 +189,14 @@ namespace SerializeDeserializeTestTask
 
                     if (datalength > 0)
                     {
-                        foreach (var ch in currentNode.Data)
+                        foreach (var ch in cn.Data)
                         {
                             s.Write(BitConverter.GetBytes(ch));
                         }
                     }
-
-                    currentNode = currentNode.Next;
-
-                } while (currentNode != null);
+                }
             }
         }
-
-
 
         public void Deserialize(Stream s)
         {
@@ -98,7 +209,7 @@ namespace SerializeDeserializeTestTask
             byte[] buffer = new byte[sizeof(int)];
 
             s.Read(buffer, 0, buffer.Length);
-            Count = BitConverter.ToInt32(buffer, 0);
+            var count = BitConverter.ToInt32(buffer, 0);
 
             int prev, next, random, datalength;
             string data;
@@ -132,12 +243,15 @@ namespace SerializeDeserializeTestTask
                     data = new string(chars.ToArray());
                 }
 
-                indexes.Add(new TempNode() { Node = new ListNode() { Data = data }, Next = next, Previous = prev, Random = random });
+                indexes.Add(new TempNode() { Node = new ListNode(data), Next = next, Previous = prev, Random = random });
 
                 buffer = new byte[sizeof(int)];
             }
 
-            if (indexes.Count != Count) throw new Exception("Stream is corrupt");
+            if (indexes.Count != count) throw new Exception("Stream is corrupt");
+
+            Init();
+
             for (int i = 0; i < indexes.Count; i++)
             {
                 FillNode(indexes, i);
@@ -149,17 +263,31 @@ namespace SerializeDeserializeTestTask
             var tempNode = indexes[i];
             var node = tempNode.Node;
 
-            if (i == 0) Head = node;
-            if (i == indexes.Count - 1) Tail = node;
-
-            if (tempNode.Previous == -1) node.Previous = null;
+            if (tempNode.Previous == -1) { node.Previous = null; Store[0] = node; _head = Store[0]; }
             else node.Previous = indexes[tempNode.Previous].Node;
 
             if (tempNode.Random == -1) node.Random = null;
             else node.Random = indexes[tempNode.Random].Node;
 
-            if (tempNode.Next == -1) node.Next = null;
+            if (tempNode.Next == -1) { node.Next = null; Store[1] = node; _tail = Store[1]; }
             else node.Next = indexes[tempNode.Next].Node;
+
+            if (node.Previous != null && node.Next != null)
+                Store.Add(node);
+        }
+
+        public List<string> GetElements()
+        {
+            List<string> o = new List<string>();
+
+            ListNode cn = Head;
+            do
+            {
+                o.Add(cn.Data);
+                cn = cn.Next;
+            } while (cn != null);
+
+            return o;
         }
     }
 }
--- Test/ListRandomTest.cs
+++ Test/ListRandomTest.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using SerializeDeserializeTestTask;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Test
@@ -8,26 +9,17 @@ namespace Test
     {
 
         [Test]
-        public void SerializeDeserialize_Normal()
+        public void SerializeDeserialize_Normal2Elements()
         {
             var lr = new ListRandom();
-            lr.Head = new ListNode();
-            lr.Tail = new ListNode();
-
-            lr.Head.Previous = null;
-            lr.Head.Random = null;
-            lr.Head.Next = lr.Tail;
-
-            lr.Tail.Previous = lr.Head;
-            lr.Tail.Next = null;
-            lr.Tail.Random = lr.Head;
+            var lr2 = new ListRandom();
 
             lr.Head.Data = "123";
             lr.Tail.Data = "234";
 
-            lr.Count = 2;
+            lr.Head.Random = lr.Tail;
+            lr.Tail.Random = lr.Head;
 
-            var lr2 = new ListRandom();
             using (var s = new MemoryStream())
             {
                 s.Seek(0, SeekOrigin.Begin);
@@ -37,18 +29,21 @@ namespace Test
             }
 
             Assert.AreEqual(lr.Count, lr2.Count);
+
             Assert.AreEqual(lr2.Tail, lr2.Head.Next);
-            Assert.AreEqual(null, lr2.Head.Random);
             Assert.AreEqual(null, lr2.Head.Previous);
+            Assert.AreEqual(lr2.Tail, lr2.Head.Random);
             Assert.AreEqual("123", lr2.Head.Data);
 
             Assert.AreEqual(null, lr2.Tail.Next);
-            Assert.AreEqual(lr2.Head, lr2.Tail.Random);
             Assert.AreEqual(lr2.Head, lr2.Tail.Previous);
+            Assert.AreEqual(lr2.Head, lr2.Tail.Random);
             Assert.AreEqual("234", lr2.Tail.Data);
 
             lr2.Head.Data = null;
             lr2.Tail.Data = null;
+            lr2.Head.Random = null;
+            lr2.Tail.Random = null;
 
             using (var s = new MemoryStream())
             {
@@ -59,15 +54,73 @@ namespace Test
             }
 
             Assert.AreEqual(lr2.Count, lr.Count);
+
             Assert.AreEqual(lr.Tail, lr.Head.Next);
-            Assert.AreEqual(null, lr.Head.Random);
             Assert.AreEqual(null, lr.Head.Previous);
+            Assert.AreEqual(null, lr.Head.Random);
             Assert.AreEqual(null, lr.Head.Data);
 
             Assert.AreEqual(null, lr.Tail.Next);
-            Assert.AreEqual(lr.Head, lr.Tail.Random);
             Assert.AreEqual(lr.Head, lr.Tail.Previous);
+            Assert.AreEqual(null, lr.Tail.Random);
             Assert.AreEqual(null, lr.Tail.Data);
         }
+
+        [Test]
+        public void SerializeDeserialize_Normal3Elements()
+        {
+            var lr = new ListRandom();
+            var lr2 = new ListRandom();
+
+            lr.Head.Data = "1";
+            lr.Tail.Data = "3";
+            lr.InsertElement(1, "2");
+
+            using (var s = new MemoryStream())
+            {
+                s.Seek(0, SeekOrigin.Begin);
+                lr.Serialize(s);
+                s.Seek(0, SeekOrigin.Begin);
+                lr2.Deserialize(s);
+            }
+
+            Assert.AreEqual(lr.Count, lr2.Count);
+
+            Assert.AreNotEqual(lr2.Tail, lr2.Head.Next);
+            Assert.AreEqual(null, lr2.Head.Previous);
+            Assert.AreEqual("1", lr2.Head.Data);
+
+        }
+
+        [Test]
+        public void SerializeDeserialize_NormalManyElements()
+        {
+            var lr = new ListRandom();
+            var lr2 = new ListRandom();
+
+            lr.Head.Data = "1";
+            lr.Tail.Data = "10";
+            lr.InsertElement(1, "9");
+            lr.InsertElement(1, "8");
+            lr.InsertElement(1, "7");
+            lr.InsertElement(1, "6");
+            lr.InsertElement(1, "5");
+            lr.InsertElement(1, "4");
+            lr.InsertElement(1, "3");
+            lr.InsertElement(1, "2");
+
+            using (var s = new MemoryStream())
+            {
+                s.Seek(0, SeekOrigin.Begin);
+                lr.Serialize(s);
+                s.Seek(0, SeekOrigin.Begin);
+                lr2.Deserialize(s);
+            }
+
+            Assert.AreEqual(lr.Count, lr2.Count);
+            CollectionAssert.AreEqual(new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" },
+                    lr2.GetElements());
+        }
+
     }
 }
